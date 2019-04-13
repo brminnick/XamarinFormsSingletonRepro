ecfd97e01d98cdf557b3c9484398c718775f89fa
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-04-13 15:25:46 -0700

    First Commit

---
+++ XamarinFormsSingletonRepro/XamarinFormsSingletonRepro/App.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinFormsSingletonRepro
+{
+public class App : Application
+{
+    public App() => MainPage = SingletonMainPage.Instance;
+}
+
+class SingletonMainPage : ContentPage
+{
+    readonly static Lazy<SingletonMainPage> _instanceHolder = new Lazy<SingletonMainPage>(() => new SingletonMainPage());
+
+    SingletonMainPage()
+    {
+        Content = new Label
+        {
+            VerticalOptions = LayoutOptions.Center,
+            HorizontalOptions = LayoutOptions.Center,
+            HorizontalTextAlignment = TextAlignment.Center,
+            VerticalTextAlignment = TextAlignment.Center,
+
+            Text = "Main Page"
+        };
+    }
+
+    public static SingletonMainPage Instance => _instanceHolder.Value;
+}
+}
